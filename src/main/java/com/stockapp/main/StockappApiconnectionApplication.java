d46058b245786bd825049df9f32e94c43d324ec2
Pedro Fanelli <pedrofanelli1@gmail.com>
2024-01-24 18:44:39 -0300

    init

---
+++ src/main/java/com/stockapp/main/StockappApiconnectionApplication.java
@@ -0,0 +1,13 @@
+package com.stockapp.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockappApiconnectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockappApiconnectionApplication.class, args);
+	}
+
+}
