3dded940069e79bd1b1043208ce15a1fd6735e97
Pedro Fanelli <pedrofanelli1@gmail.com>
2024-02-03 16:30:10 -0300

    live update, day by day

--- src/main/java/com/stockapp/main/controllers/SSEController.java
+++ src/main/java/com/stockapp/main/controllers/SSEController.java
@@ -1,17 +1,88 @@
 package com.stockapp.main.controllers;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.atomic.AtomicInteger;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.reactive.function.client.ClientResponse;
+import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
-@RestController
+import com.stockapp.main.DTOs.Aggregates;
+import com.stockapp.main.DTOs.AggregatesResult;
+
+import reactor.core.publisher.Mono;
+
+@Controller
 public class SSEController {
 
 	private final List<SseEmitter> emitters = new CopyOnWriteArrayList<>();
+	    
+    private AtomicInteger intAtomico = new AtomicInteger(0);
+    
+    private AggregatesResult[] arrResults;
+	
+	@Autowired
+    WebClient webClient;
+	
+	private Mono<? extends Throwable> handleClientError(ClientResponse clientResponse) {
+        // Handle client error (4xx) here
+        // primero entramos aca, es como que ataja el error de http, y devolvemos un Mono, atajado al subscribir
+            System.out.println("DENTRO DE HANDLE CLIENT ERROR");
+            return Mono.error(new Exception("SOY LA EXCEPCION de client: "+clientResponse.statusCode())); // podriamos lanzar excepcion Custom
+            //return Mono.error(new CustomClientException(clientResponse.statusCode()));
+        }
+
+        private Mono<? extends Throwable> handleServerError(ClientResponse serverResponse) {
+            // Handle server error (5xx) here
+            System.out.println("DENTRO DE HANDLE server ERROR");
+            return Mono.error(new Exception("SOY LA EXCEPCION de server: "+serverResponse.statusCode()));
+        }
+	
+	@GetMapping("/livegraph")
+	public String graph(Model model) {
+		
+		//[1643121000000,158.98,162.76,157.02,159.78,115798400]
+        // DATE - OPEN - HIGH - LOW - CLOSE - VOLUME
+        
+        ArrayList<Aggregates> listado = new ArrayList<>();
+        
+        Aggregates response = webClient
+								.get()
+								.uri("/v2/aggs/ticker/AAPL/range/1/day/2023-01-25/2024-01-25?adjusted=true&sort=asc")
+								.retrieve()
+                                .onStatus(status -> status.is4xxClientError(), clientResponse -> handleClientError(clientResponse))
+                                .onStatus(status -> status.is5xxServerError(), serverResponse -> handleServerError(serverResponse))
+                                //.onStatus(HttpStatus::isError, clientResponse -> handleAnyError(clientResponse))
+                                .bodyToMono(Aggregates.class) //throws JsonProcessingException si pifiamos en el procesamiento en mi DTO
+                                .block();
+        
+        
+        this.arrResults = response.getResults();
+        
+        AggregatesResult inicial = this.arrResults[this.intAtomico.intValue()];
+        
+        //this.intAtomico.incrementAndGet();
+        
+        model.addAttribute("listadito", List.of(inicial));
+        
+        
+        
+        
+        //model.addAttribute("listadito", response.getResults());
+        model.addAttribute("name", "Peterrrr");
+		
+		return "Graph";
+	}
+	
+	
 	
 	@GetMapping("/emitter")
 	SseEmitter getEmitter() {
@@ -60,8 +131,18 @@ public class SSEController {
         
         for (SseEmitter emitter : emitters) {
             try {
-            	List<Long> arrDatos = List.of(1674536400000L, 100L, 102L, 98L, 101L, 60800355L);
-                emitter.send(SseEmitter.event().data(arrDatos));
+            	//List<Long> arrDatos = List.of(1674536400000L, 100L, 102L, 98L, 101L, 60800355L);
+                //emitter.send(SseEmitter.event().data(arrDatos));
+            	
+            	int newIndex = this.intAtomico.incrementAndGet();
+            	
+            	if (newIndex >= this.arrResults.length) {
+            		emitter.complete();
+            	} else {
+                	emitter.send(SseEmitter.event().data(this.arrResults[this.intAtomico.get()]));
+            	}
+            	
+            	
                 
             } catch (Exception e) {
                 // Handle exceptions (e.g., closed connections)
