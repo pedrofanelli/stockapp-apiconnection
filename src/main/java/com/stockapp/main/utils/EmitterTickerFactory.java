17a57c540d09c66ddaafc964a99ba6079462143c
Pedro Fanelli <pedrofanelli1@gmail.com>
2024-02-14 19:53:24 -0300

    few classes

--- src/main/java/com/stockapp/main/utils/EmitterTickerFactory.java
+++ src/main/java/com/stockapp/main/utils/EmitterTickerFactory.java
@@ -1,6 +1,5 @@
 package com.stockapp.main.utils;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -8,13 +7,13 @@ public class EmitterTickerFactory {
 
 	// constructor will not be necesary
 	
-	@Autowired
-	EmitterTickerContainer emitterTickerContainer;
 	
 	public EmitterTicker createEmitterTicker(String ticker) {
 		
-		// check if the ticker already exists
+		return new EmitterTicker(ticker);
 		
 	}
+	
+	
 	
 }
---
+++ src/main/java/com/stockapp/main/utils/EmitterTickerManager.java
@@ -0,0 +1,14 @@
+package com.stockapp.main.utils;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class EmitterTickerManager {
+
+	@Autowired
+	EmitterTickerContainer container;
+	
+	
+	
+}
